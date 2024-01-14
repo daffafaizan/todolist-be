3bb80ad818821658bd9e65d70d68fcf31f1e39d6
Daffa <daffamfzn@gmail.com>
2024-01-14 14:58:02 +0700

    feat: integrated database to app.js

--- src/app.js
+++ src/app.js
@@ -1,21 +1,46 @@
 // 3rd Party Modules
 const express = require("express");
+const bodyParser = require("body-parser");
+const sequelize = require("./utils/database.js");
 require("dotenv/config");
 
 // Local Modules
 const mainRoutes = require("./routes/mainRouter.js");
 const env = process.env;
+const PORT = env.PORT;
 
 // Server Initialization
-const PORT = env.PORT;
 app = express();
 
 // Middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Server Setup
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH");
+  next();
+});
 
 // Routes
 app.use("/api/v1", mainRoutes);
 
-// Server listen and Database connection
-app.listen(PORT, () => {
-  console.log(`Server has started on port: ${PORT}`);
+// Error Handling
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
 });
+
+// Sync Database and Server Connection
+sequelize
+  .sync()
+  .then((result) => {
+    console.log("Database connected!");
+    app.listen(PORT, () => {
+      console.log(`Server has started on port:${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
--- src/utils/database.js
+++ src/utils/database.js
@@ -1,11 +1,12 @@
 // 3rd Party Modules
 const Sequelize = require("sequelize");
+require("dotenv/config");
 
 // Local Modules
 const env = process.env;
 
 // Database Initialization
-const sequelize = new Sequelize(env.DB_NAME, env.DB_USER, env.DB_PASSWORD, {
+const sequelize = new Sequelize(env.DB_NAME, env.DB_USERNAME, env.DB_PASSWORD, {
   host: env.DB_HOST,
   dialect: "postgres",
 });
