46bd7d52a642aaa330e9c2125bbe6f73fb7b2793
Daffa <daffamfzn@gmail.com>
2024-01-15 09:50:52 +0700

    feat: made todolist controller logic

--- src/controllers/todolistController.js
+++ src/controllers/todolistController.js
@@ -2,20 +2,68 @@
 const todolistService = require("../services/todolistService.js");
 
 // Controllers
-const getAllTodolists = () => {
-  return null;
+const getAllTodolists = async (req, res, next) => {
+  try {
+    const todolists = await todolistService.getAllTodolists();
+    res.status(200).json({ todolists: todolists });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
-const getTodolistById = () => {
-  return null;
+const getTodolistById = async (req, res, next) => {
+  const id = req.params.id;
+  try {
+    const todolist = await todolistService.getTodolistById(id);
+    if (!todolist) {
+      return res.status(404).json({ message: "Todolist not found!" });
+    }
+    res.status(200).json({ todolist: todolist });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
-const createTodolist = () => {
-  return null;
+const createTodolist = (req, res, next) => {
+  try {
+    const todolist = todolistService.createTodolist(req);
+    res.status(201).json({
+      message: "Todolist created!",
+      todolist: todolist,
+    });
+  } catch (err) {
+    if (err.message.includes("required field")) {
+      res.status(400).json({ error: err.message });
+    }
+    console.log(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
-const updateTodolistById = () => {
-  return null;
+const updateTodolistById = async (req, res, next) => {
+  const id = req.params.id;
+  try {
+    const todolist = await todolistService.updateTodolistById(id, req);
+    if (!todolist) {
+      return res.status(404).json({ message: "Todolist not found!" });
+    }
+    res.status(201).json({ message: "Todolist updated!" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
-const deleteTodolistById = () => {
-  return null;
+const deleteTodolistById = async (req, res, next) => {
+  const id = req.params.id;
+  try {
+    const todolist = await todolistService.deleteTodolistById(id);
+    if (!todolist) {
+      return res.status(404).json({ message: "Todolist not found!" });
+    }
+    res.status(200).json({ message: "Todolist deleted!" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 
 module.exports = {
