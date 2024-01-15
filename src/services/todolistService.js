e399ab2000e83139d9564085fb0cc7c6c6f592af
Daffa <daffamfzn@gmail.com>
2024-01-15 09:16:40 +0700

    feat: defined todolist services

---
+++ src/services/todolistService.js
@@ -0,0 +1,27 @@
+// Local Modules
+const Todolist = require("../models/user.js");
+
+// Services
+const getAllTodolists = () => {
+  return null;
+};
+const getTodolistById = () => {
+  return null;
+};
+const createTodolist = () => {
+  return null;
+};
+const updateTodolistById = () => {
+  return null;
+};
+const deleteTodolistById = () => {
+  return null;
+};
+
+module.exports = {
+  getAllTodolists,
+  getTodolistById,
+  createTodolist,
+  updateTodolistById,
+  deleteTodolistById,
+};
