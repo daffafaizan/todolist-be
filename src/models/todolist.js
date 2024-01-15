f3e80538843f29de48ddc52d77cd96579e14ac80
Daffa <daffamfzn@gmail.com>
2024-01-15 09:13:29 +0700

    feat: defined todolist model

---
+++ src/models/todolist.js
@@ -0,0 +1,34 @@
+// 3rd Party Modules
+const Sequelize = require("sequelize");
+
+// Local Modules
+const db = require("../utils/database.js");
+
+// Model Initialization
+const todolist = db.define("todolist", {
+  id: {
+    type: Sequelize.BIGINT,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  content: {
+    type: Sequelize.TEXT("long"),
+  },
+  priority: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  completed: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+  },
+});
+
+module.exports = {
+  todolist,
+};
