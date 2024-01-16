c59e8eec3c5570ba660901e276bd0e0f820bb17a
Daffa <daffamfzn@gmail.com>
2024-01-16 22:02:02 +0700

    feat: migrated from conventional database fields to conn string

--- src/utils/database.js
+++ src/utils/database.js
@@ -6,8 +6,7 @@ require("dotenv/config");
 const env = process.env;
 
 // Database Initialization
-const sequelize = new Sequelize(env.DB_NAME, env.DB_USERNAME, env.DB_PASSWORD, {
-  host: env.DB_HOST,
+const sequelize = new Sequelize(env.DB_CONNECTION_STRING, {
   dialect: "postgres",
 });
 
